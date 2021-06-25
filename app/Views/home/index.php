de95923f3d5312f259bf9d475779f1fa54630ee1
Dede Nurhadi Putra <gderyx2@gmail.com>
2021-06-25 17:42:36 +0700

    menambahkan section galery, contact dan footer

--- app/Views/home/index.php
+++ app/Views/home/index.php
@@ -20,16 +20,110 @@
             </div>
         </div>
         <div class="row justify-content-center fs-5 text-center">
-            <div class="col-5">
+            <div class="col-md-5 mb-3">
                 Lorem ipsum dolor sit amet consectetur adipisicing elit. Eius minus esse dolorum expedita repellat consectetur! Laudantium repellendus, voluptas facilis enim porro quasi voluptate! Ex repellendus, perspiciatis veniam exercitationem deserunt ab.
             </div>
-            <div class="col-5">
+            <div class="col-md-5 mb-2">
                 Lorem ipsum dolor sit amet consectetur adipisicing elit. Animi aliquam dolore cumque perferendis illo modi in ullam quas corrupti earum magni atque nobis porro voluptas accusantium, eaque saepe rem laboriosam voluptatum? asperiores quaerat.
             </div>
         </div>
     </div>
     <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1440 320">
         <path fill="#e2edff" fill-opacity="1" d="M0,160L40,149.3C80,139,160,117,240,128C320,139,400,181,480,176C560,171,640,117,720,117.3C800,117,880,171,960,192C1040,213,1120,203,1200,192C1280,181,1360,171,1400,165.3L1440,160L1440,320L1400,320C1360,320,1280,320,1200,320C1120,320,1040,320,960,320C880,320,800,320,720,320C640,320,560,320,480,320C400,320,320,320,240,320C160,320,80,320,40,320L0,320Z"></path>
+    </svg>
+</section>
+
+<!-- Gallery -->
+<section id="gallery">
+    <div class="container">
+        <div class="row text-center mb-3">
+            <div class="col">
+                <h2>Gallery</h2>
+            </div>
+        </div>
+        <div class="row justify-content-center">
+            <div class="col-md-4 mb-3">
+                <div class="card">
+                    <img src="/img/nubelson-fernandes--Xqckh_XVU4-unsplash.jpg" class="card-img-top" alt="Gallery1">
+                    <div class="card-body">
+                        <p class="card-text">Photo by <a href="https://unsplash.com/@nublson?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText">Nubelson Fernandes</a> on <a href="https://unsplash.com/?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText">Unsplash</a>
+                        </p>
+                    </div>
+                </div>
+            </div>
+            <div class="col-md-4 mb-3">
+                <div class="card">
+                    <img src="/img/fernando-hernandez-JdoofvUDUwc-unsplash.jpg" class="card-img-top" alt="Gallery2">
+                    <div class="card-body">
+                        <p class="card-text">Photo by <a href="https://unsplash.com/@_ferh97?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText">Fernando Hernandez</a> on <a href="https://unsplash.com/s/photos/developer?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText">Unsplash</a>
+                        </p>
+                    </div>
+                </div>
+            </div>
+            <div class="col-md-4 mb-3">
+                <div class="card">
+                    <img src="/img/altumcode-oZ61KFUQsus-unsplash.jpg" class="card-img-top" alt="Gallery3">
+                    <div class="card-body">
+                        <p class="card-text">Photo by <a href="https://unsplash.com/@altumcode?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText">AltumCode</a> on <a href="https://unsplash.com/collections/2718756/blog?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText">Unsplash</a>
+                        </p>
+                    </div>
+                </div>
+            </div>
+            <div class="col-md-4 mb-3">
+                <div class="card">
+                    <img src="/img/huy-phan-bXy37LWsGjU-unsplash.jpg" class="card-img-top" alt="Gallery4">
+                    <div class="card-body">
+                        <p class="card-text">Photo by <a href="https://unsplash.com/@huyphan2602?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText">Huy Phan</a> on <a href="https://unsplash.com/t/technology?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText">Unsplash</a>
+                        </p>
+                    </div>
+                </div>
+            </div>
+            <div class="col-md-4 mb-3">
+                <div class="card">
+                    <img src="/img/fabian-albert-uwL_JvIhtLM-unsplash.jpg" class="card-img-top" alt="Gallery5">
+                    <div class="card-body">
+                        <p class="card-text">Photo by <a href="https://unsplash.com/@fabiraw?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText">Fabian Albert</a> on <a href="https://unsplash.com/t/technology?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText">Unsplash</a>
+                        </p>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1440 320">
+        <path fill="#fff" fill-opacity="1" d="M0,160L48,176C96,192,192,224,288,208C384,192,480,128,576,122.7C672,117,768,171,864,170.7C960,171,1056,117,1152,106.7C1248,96,1344,128,1392,144L1440,160L1440,320L1392,320C1344,320,1248,320,1152,320C1056,320,960,320,864,320C768,320,672,320,576,320C480,320,384,320,288,320C192,320,96,320,48,320L0,320Z"></path>
+    </svg>
+</section>
+
+<!-- Contact -->
+<section id="contact">
+    <div class="container">
+        <div class="row text-center mb-3">
+            <div class="col">
+                <h2>Contact Me</h2>
+            </div>
+        </div>
+        <div class="row justify-content-center">
+            <div class="col-md-6">
+                <form>
+                    <div class="mb-3">
+                        <label for="name" class="form-label">Nama</label>
+                        <input type="text" class="form-control" id="name" aria-describedby="name">
+                    </div>
+                    <div class="mb-3">
+                        <label for="email" class="form-label">Email</label>
+                        <input type="email" class="form-control" id="email" aria-describedby="email">
+                    </div>
+                    <div class="mb-3">
+                        <label for="pesan" class="form-label">Pesan</label>
+                        <textarea class="form-control" id="pesan" rows="3"></textarea>
+                    </div>
+                    <button type="submit" class="btn btn-primary">Kirim</button>
+                </form>
+            </div>
+        </div>
+    </div>
+    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1440 320">
+        <path fill="#0d6efd" fill-opacity="1" d="M0,224L60,224C120,224,240,224,360,218.7C480,213,600,203,720,208C840,213,960,235,1080,240C1200,245,1320,235,1380,229.3L1440,224L1440,320L1380,320C1320,320,1200,320,1080,320C960,320,840,320,720,320C600,320,480,320,360,320C240,320,120,320,60,320L0,320Z"></path>
     </svg>
 </section>
 
